f8df79186739957a02fd88f9c877ef04236ea874
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-18 18:06:46 +0100

    filters functionality changed

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,6 +12,7 @@ const Main = () => {
 		todo: todo,
 	};
 	const [allTodos, setAllTodos] = useState([]);
+	const [diffTodos, setDiffTodos] = useState([]);
 
 	const handleAddTodo = (ev) => {
 		ev.preventDefault();
@@ -25,20 +26,47 @@ const Main = () => {
 	let allPrev = useRef(allTodos);
 	let completePrev = allTodos.filter((each) => each.completed);
 	useEffect(() => {
-		setAllTodos((prev) => prev);
-	}, [allTodos.completed]);
+		// setAllTodos((prev) => prev);
+		setDiffTodos((prev) => {
+			function addName(arr) {
+				return arr.map((each) => ({ ...each, name: arr }));
+			}
+			let completeArr = allTodos
+				.filter((each) => each.completed)
+				.map((each) => ({ ...each, name: "completeArr" }));
+			let activeArr = allTodos
+				.filter((each) => each.completed === false)
+				.map((each) => ({ ...each, name: "activeArr" }));
+			let allArr = allTodos.map((each) => ({ ...each, name: "allArr" }));
 
+			return [...prev, completeArr, activeArr, allArr];
+		});
+		// clean up function
+		// () => {
+		// 	return setDiffTodos((prev)=>{
+		// 		prev.map(each=> {
+
+		// 		})
+		// 	})
+
+		// }
+		console.log(diffTodos);
+	}, [allTodos.completed]);
+	console.log(diffTodos);
 	const handleDisplays = (e) => {
 		console.log(e.target.innerHTML);
 		let content = e.target.innerHTML;
-		content === "all" && setAllTodos((prev) => allPrev.current);
-		content === "active" &&
-			setAllTodos((prev) => {
-				let displaying = prev.filter((el) => el.completed === false);
-				return displaying;
-			});
+		// content === "all" && setAllTodos((prev) => allPrev.current);
+		// content === "active" &&
+		// 	setAllTodos((prev) => {
+		// 		let displaying = prev.filter((el) => el.completed === false);
+		// 		return displaying;
+		// 	});
 
-		content === "completed" && setAllTodos(completePrev);
+		// content === "completed" && setAllTodos(completePrev);
+
+		content === "all" &&
+			setAllTodos(diffTodos.find((each) => each.name === "completeArr"));
 		// setAllTodos(allPrev.current);
 	};
 
